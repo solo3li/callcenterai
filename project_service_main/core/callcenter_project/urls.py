05410f949a0cd9dc5460dd139fdb81c718638b5b
solo3li <hamed3alii.3@gmail.com>
2026-09-09 17:05:25 +0300

    feat: implement base templates, styling, dashboard layout, and login authentication views

--- project_service_main/core/callcenter_project/urls.py
+++ project_service_main/core/callcenter_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Authentication
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # Dashboard
+    path('', TemplateView.as_view(template_name='core/dashboard.html'), name='dashboard'),
 ]
