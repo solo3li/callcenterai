86f413ccb1d7e03619ce60fa8dde32200c6a658c
solo3li <hamed3alii.3@gmail.com>
2026-09-10 04:45:32 +0300

    feat: register custom User model in Django admin with organization and agent group fields

--- project_service_main/core/users/admin.py
+++ project_service_main/core/users/admin.py
@@ -4,9 +4,9 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display  = ('username', 'email', 'organization', 'role', 'is_active', 'is_staff', 'is_superuser')
-    list_filter   = ('role', 'is_active', 'is_staff', 'organization')
+    list_display  = ('username', 'email', 'organization', 'is_active', 'is_staff', 'is_superuser')
+    list_filter   = ('is_active', 'is_staff', 'organization')
     search_fields = ('username', 'email')
     fieldsets = BaseUserAdmin.fieldsets + (
-        ('Call Center', {'fields': ('organization', 'role')}),
+        ('Call Center', {'fields': ('organization', 'agent_groups')}),
     )
